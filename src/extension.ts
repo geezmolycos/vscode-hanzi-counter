98b768ecad4373305220918748badfbe20902cf0
geezmolycos <djy0212@foxmail.com>
2023-02-12 00:42:35 +0800

    实现文件字数缓存的架构

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,26 @@
-
-import {window, workspace, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument, MarkdownString, WorkspaceConfiguration, TextEditor} from 'vscode';
+import {
+    Disposable, Range,
+    window, workspace, commands,
+    ExtensionContext, StatusBarItem, StatusBarAlignment,
+    MarkdownString, WorkspaceConfiguration, TextEditor,
+    TextDocument, TextDocumentChangeEvent, TextDocumentContentChangeEvent
+} from 'vscode';
 
 // this method is called when your extension is activated. activation is
 // controlled by the activation events defined in package.json
 export function activate(context: ExtensionContext) {
 
     // create a new word counter
-    let wordCounter = new WordCounter(workspace.getConfiguration());
-    let controller = new WordCounterController(wordCounter);
+    let counter = new Counter(workspace.getConfiguration());
+    let controller = new CounterController(counter);
 
     // add to a list of disposables which are disposed when this extension
     // is deactivated again.
     context.subscriptions.push(controller);
-    context.subscriptions.push(wordCounter);
+    context.subscriptions.push(counter);
 
-    let disposable = commands.registerCommand('vscode-hanzi-counter.clickedTooltip', () => {
-        wordCounter.updateWordCount(true);
+    let disposable = commands.registerCommand('vscode-hanzi-counter.changeTooltip', (templateName) => {
+        console.log(templateName);
 	});
 
 	context.subscriptions.push(disposable);
@@ -44,80 +49,162 @@ function compileTemplateFunction(template: string): [string[], Function] {
     return [parameters, new Function(...parameters, functionBody)];
 }
 
-class WordCounter {
+class CachedLineCount {
+    private _lines: number[];
+    private _sum: number;
 
-    private readonly _counterRegex: Map<string, RegExp>;
+    constructor(lines: number[]){
+        this._lines = lines;
+        this._sum = 0;
+        this.recalculateSum();
+    }
 
-    private readonly _statusBarTemplate: Function;
-    private readonly _tooltipTemplate: Function;
-    private readonly _clickedTooltipTemplate: Function;
+    public getSum(){
+        return this._sum;
+    }
 
-    private readonly _statusBarKeys: string[];
-    private readonly _tooltipKeys: string[];
-    private readonly _clickedTooltipKeys: string[];
+    public recalculateSum(){
+        this._sum = this._lines.reduce((a, b) => a + b, 0);
+    }
 
-    private readonly _statusBarItem: StatusBarItem;
+    public replace(start: number, end: number, newLineCounts: number[]){
+        let removed = this._lines.splice(start, end - start, ...newLineCounts);
+        let removedTotalCount = removed.reduce((a, b) => a + b, 0);
+        this._sum -= removedTotalCount;
+        let addedTotalCount = newLineCounts.reduce((a, b) => a + b, 0);
+        this._sum += addedTotalCount;
+    }
+}
 
-    constructor(configuration: WorkspaceConfiguration) {
-        const counterRegexString = new Map(Object.entries(
-            configuration.get('vscode-hanzi-counter.counterRegex') as object
-        ));
-        this._counterRegex = new Map();
-        for (let [k, v] of counterRegexString){
-            this._counterRegex.set(k, new RegExp(v, 'gu'));
+class DocumentCounter {
+
+    private _counter: Counter;
+
+    private _document: TextDocument;
+    private _cachedLineCounts: Map<string, CachedLineCount>;
+    
+    private _enabled!: boolean;
+    private _statusBarTemplateName!: string;
+    private _tooltipTemplateName!: string;
+
+    constructor(counter: Counter, document: TextDocument){
+        this._counter = counter;
+        this._document = document;
+        this._cachedLineCounts = new Map();
+        this.updateConfiguration();
+    }
+
+    public updateConfiguration(){
+        let configuration = workspace.getConfiguration('', this._document);
+        this._enabled = configuration.get('vscode-hanzi-counter.enabled') as boolean;
+        this._statusBarTemplateName = configuration.get('vscode-hanzi-counter.statusBarTemplateName') as string;
+        this._tooltipTemplateName = configuration.get('vscode-hanzi-counter.tooltipTemplateName') as string;
+    }
+
+    public recalculateCount(regexName: string){
+        // calculate and store count for each line
+        let regex = this._counter.regexes.get(regexName);
+        if (!regex){
+            return;
+        }
+        let lineCount = [];
+        for (let lineNumber = 0; lineNumber < this._document.lineCount; ++lineNumber){
+            let line = this._document.lineAt(lineNumber);
+            let lineText = line.text;
+            let matchCount = (lineText.match(regex) ?? []).length;
+            lineCount.push(matchCount);
+        }
+        this._cachedLineCounts.set(regexName, new CachedLineCount(lineCount));
+    }
+
+    public onContentChange(event: TextDocumentContentChangeEvent){
+        console.log(event);
+        this.updateStatusBarItem();
+    }
+
+    public getCount(regexName: string){
+        if (!this._cachedLineCounts.has(regexName)){
+            this.recalculateCount(regexName);
+        }
+        return this._cachedLineCounts.get(regexName)?.getSum();
+    }
+
+    public updateStatusBarItem(tooltipTemplateName?: string){
+        if (!this._enabled){
+            this._counter.changeStatusBarItem(false);
+            return;
+        } else {
+            this._counter.changeStatusBarItem(true);
         }
 
-        [this._statusBarKeys, this._statusBarTemplate] = compileTemplateFunction(configuration.get<string>('vscode-hanzi-counter.statusBarTemplate') ?? '');
-        [this._tooltipKeys, this._tooltipTemplate] = compileTemplateFunction(configuration.get<string>('vscode-hanzi-counter.tooltipTemplate') ?? '');
-        [this._clickedTooltipKeys, this._clickedTooltipTemplate] = compileTemplateFunction(configuration.get<string>('vscode-hanzi-counter.clickedTooltipTemplate') ?? '');
+        let evalTemplate = (regexNames: string[], templateFunction: Function) =>
+            templateFunction(...regexNames.map(s => this.getCount(s)));
+           
+        let statusBarTemplate = this._counter.templates.get(this._statusBarTemplateName);
+        if (statusBarTemplate !== undefined){
+            let [statusBarRegexNames, statusBarTemplateFunction] = statusBarTemplate;
+            this._counter.updateStatusBarItem(evalTemplate(statusBarRegexNames, statusBarTemplateFunction), undefined);
+        }
 
-        this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Right, 105); // left of text attributes(ln, col, spaces, encoding, etc)
+        tooltipTemplateName ??= this._tooltipTemplateName;
+        let tooltipTemplate = this._counter.templates.get(tooltipTemplateName);
+        if (tooltipTemplate !== undefined){
+            let [tooltipRegexNames, tooltipTemplateFunction] = tooltipTemplate;
+            this._counter.updateStatusBarItem(undefined, evalTemplate(tooltipRegexNames, tooltipTemplateFunction));
+        }
+    }
+}
+
+class Counter {
+
+    public readonly regexes: Map<string, RegExp>;
+    public readonly templates: Map<string, [string[], Function]>;
+
+    private _statusBarItem: StatusBarItem;
+
+    constructor(configuration: WorkspaceConfiguration) {
+        const regexStrings = new Map(Object.entries(
+            configuration.get('vscode-hanzi-counter.regexes') as object
+        ));
+        this.regexes = new Map();
+        for (let [k, v] of regexStrings){
+            this.regexes.set(k, new RegExp(v, 'gu'));
+        }
+
+        const templateStrings = new Map(Object.entries(
+            configuration.get('vscode-hanzi-counter.templates') as object
+        ));
+        this.templates = new Map();
+        for (let [k, v] of templateStrings){
+            this.templates.set(k, compileTemplateFunction(v));
+        }
+
+        this._statusBarItem = window.createStatusBarItem(
+            configuration.get('vscode-hanzi-counter.alignment') === 'left'
+                ? StatusBarAlignment.Left : StatusBarAlignment.Right,
+            configuration.get('vscode-hanzi-counter.priority') ?? 105); // default left of text attributes(ln, col, spaces, encoding, etc)
         this._statusBarItem.name = 'Hanzi Counter';
     }
 
-    public updateWordCount(clicked = false) {
-
-        // Get the current text editor
-        const editor = window.activeTextEditor;
-        if (!editor) {
+    public changeStatusBarItem(show: boolean){
+        if (show){
+            this._statusBarItem.show();
+        } else {
             this._statusBarItem.hide();
-            return;
         }
-
-        let cachedKeys = new Map(); // cache common keys for different templates
-        let [statusBarArgs, tooltipArgs] = [this._statusBarKeys, clicked ? this._clickedTooltipKeys : this._tooltipKeys].map(ks => ks.map(key => {
-            if (cachedKeys.has(key)){
-                return cachedKeys.get(key);
-            } else {
-                let regex = this._counterRegex.get(key);
-                if (regex === undefined){
-                    throw new Error('undefined counter regex key');
-                }
-                let count = this._getWordCount(editor, regex);
-                cachedKeys.set(key, count);
-                return count;
-            }
-        }));
-
-        // Update the status bar
-        this._statusBarItem.text = this._statusBarTemplate(...statusBarArgs);
-        let ms = new MarkdownString((clicked ? this._clickedTooltipTemplate : this._tooltipTemplate)(...tooltipArgs));
-        ms.isTrusted = true;
-        ms.supportHtml = true;
-        ms.supportThemeIcons = true;
-        this._statusBarItem.tooltip = ms;
-        this._statusBarItem.show();
     }
 
-    public _getWordCount(editor: TextEditor, regex: RegExp): number {
-        let docContent;
-        if (editor.selection.isEmpty) {
-            docContent = editor.document.getText()
-        } else {
-            docContent = editor.document.getText(editor.selection);
+    public updateStatusBarItem(text?: string, tooltipText?: string){
+        if (text){
+            this._statusBarItem.text = text;
         }
-        let wordCount = (docContent.match(regex) || []).length;
-        return wordCount;
+        if (tooltipText){
+            let ms = new MarkdownString(tooltipText);
+            ms.isTrusted = true;
+            ms.supportHtml = true;
+            ms.supportThemeIcons = true;
+            this._statusBarItem.tooltip = ms;
+        }
     }
 
     public dispose() {
@@ -125,26 +212,53 @@ class WordCounter {
     }
 }
 
-class WordCounterController {
+class CounterController {
 
-    private _wordCounter: WordCounter;
+    private _counter: Counter;
+    private _documentCounters: Map<TextDocument, DocumentCounter>;
     private _disposable: Disposable;
 
-    constructor(wordCounter: WordCounter) {
-        this._wordCounter = wordCounter;
-        this._wordCounter.updateWordCount();
+    constructor(counter: Counter) {
+        this._counter = counter;
+        this._documentCounters = new Map();
 
         // subscribe to selection change and editor activation events
         let subscriptions: Disposable[] = [];
-        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
-        window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
+        workspace.onDidOpenTextDocument(this._onDidOpenTextDocument, this, subscriptions);
+        workspace.onDidCloseTextDocument(this._onDidCloseTextDocument, this, subscriptions);
+        workspace.onDidChangeTextDocument(this._onDidChangeTextDocument, this, subscriptions);
+        window.onDidChangeTextEditorSelection(this._updateStatusBarItem, this, subscriptions);
+        window.onDidChangeActiveTextEditor(this._updateStatusBarItem, this, subscriptions);
+
+        // some documents may be opened before we can register handler
+        for (let document of workspace.textDocuments){
+            this._onDidOpenTextDocument(document);
+        }
 
         // create a combined disposable from both event subscriptions
         this._disposable = Disposable.from(...subscriptions);
     }
 
-    private _onEvent(event: any) {
-        this._wordCounter.updateWordCount();
+    private _onDidOpenTextDocument(document: TextDocument) {
+        console.log(`open text document ${document.uri}`);
+        this._documentCounters.set(document, new DocumentCounter(this._counter, document));
+    }
+    
+    private _onDidCloseTextDocument(document: TextDocument) {
+        console.log(`close text document ${document.uri}`);
+        this._documentCounters.delete(document);
+    }
+    
+    private _onDidChangeTextDocument(event: TextDocumentChangeEvent) {
+        console.log(`change text document ${event.document.uri}`);
+        this._documentCounters.get(event.document)?.onContentChange(event.contentChanges[0]);
+    }
+
+    private _updateStatusBarItem(event: any){
+        let currentDocument = window.activeTextEditor?.document;
+        if (currentDocument){
+            this._documentCounters.get(currentDocument)?.updateStatusBarItem();
+        }
     }
 
     public dispose() {
